1eb00f0989482ba7928a725e5fe80abd6a24c3e8
David Winer <davidjwiner@davids-mbp.home>
2015-05-11 23:54:09 -0400

    .gitignore is now working

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "03O0RwgZSrvHbHDfxaGGZL57x",
-    consumer_secret: "rK7aRpuouMBOg5lvIXDWidCka1Alb9uiReJUpPhjX70n2FlNll",
-    access_token: "52306095-yBVO3cDzgfsynZ8o6wiiJM70FuaeOiQXRiTdzuhfK",
-    access_token_secret: "e1TwXR20CdVZfRsHVperMaoy1IYhHmiDFtkwwlml1GwSk"
-}
